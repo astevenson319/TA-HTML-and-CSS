98c5d4967e7e811219d5f3cb4d9a38672546cb50
astevenson319 <129712740+astevenson319@users.noreply.github.com>
2023-04-15 19:54:06 -0700

    4 15 23 updates
    Progressing material on use of color and images, and a side project version of the "Irish", showing Celtic solidarity from the Scots side of the arena.

---
+++ Basic_HTML_and_Javascript_1/Basic_JavaScript_1.js
@@ -0,0 +1,6 @@
+
+	function My_First_Function() {
+	var String = "Kiss me, I'm Irish (and not at all ethnistically prejudiced)!";
+	var result = String.fontcolor("green");
+	document.getElementById("Irish").innerHTML = result;
+	}
---
+++ Basic_HTML_and_Javascript_1/Hello_World.js
@@ -0,0 +1,1 @@
+alert("Hello, World! I am an Alert Box! Woo hoo!");
